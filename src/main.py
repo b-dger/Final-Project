72b56651b945b0f196966947791faecf371ec605
b-dger <113070423+b-dger@users.noreply.github.com>
2025-04-28 11:38:48 -0400

    Update main.py

--- src/main.py
+++ src/main.py
@@ -2,17 +2,21 @@
 
 from advisor_matcher import inputAdvisors, assignAdvisors
 from student_data import inputStudents
+from fileio import save_as_csv, save_as_json
 
 def printAssignments(assignments):
-    print("\nStudent Advisor Assignments:")
+    print("\n=== Student–Advisor Assignments ===")
     for name, major, advisor in assignments:
-        print(f"Student: {name} | Major: {major} | Advisor: {advisor}")
+        print(f"• {name:<20} | {major:<17} | {advisor}")
+    print()
 
 def main():
     advisorDirectory = inputAdvisors()
     students = inputStudents()
     assignments = assignAdvisors(students, advisorDirectory)
     printAssignments(assignments)
+    save_as_csv(assignments)
+    save_as_json(assignments)
 
 if __name__ == "__main__":
     main()
